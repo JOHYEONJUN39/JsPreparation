929d6d1787d47429d54acf6c448b3eaf06b5f9c1
NEMURENAIYORU <lhslhs301@gmail.com>
2023-04-07 11:36:28 +0900

    Docs : update JsSection

--- udemy-Study/jsSection/test.js
+++ udemy-Study/jsSection/test.js
@@ -482,3 +482,50 @@
 // --- 화살표 함수 ---
 
 
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+
+// function print(element) {
+//     console.log(element);
+// }
+
+// print(number[0]);
+// print(number[1]);
+// forEach는 함수를 받아서 그 함수를 배열의 요소 하나하나에 대입해준다.
+// number.forEach(function (element) {
+//     console.log(element);
+// });
+
+// for (let elem of number) {
+//     console.log(elem);
+// }
+
+const movies = [ 
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    
+    {
+        title: 'Alien',
+        score: 90
+    }
+]
+
+// Amadeus = 99/100
+// forEach가 하나의 요소해대해 함수를 실행 함수내용은 아래와 같음
+movies.forEach(function(movie) {
+    console.log(`${movie.title} - ${movie.score}/100`)
+})
+// for...of 가 나온이후로는 위와같이는 잘 쓰지않는다. (String에서도 사용가능하기떄문에)
+
